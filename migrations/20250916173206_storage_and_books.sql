3939ebac579def9608f13164457cc2dee32e2306
Syeda Hoorain Ali <syedahoorainali8@gmail.com>
2025-10-06 01:25:48 +0500

    update

--- migrations/20250916173206_storage_and_books.sql
+++ migrations/20250916173206_storage_and_books.sql
@@ -19,22 +19,22 @@ ON CONFLICT (id) DO NOTHING;
 -- POLICIES
 CREATE POLICY "Authenticated users can upload files"
 ON storage.objects FOR INSERT TO authenticated WITH CHECK (
-  bucket_id = 'files' AND OWNER = auth.uid()
+  bucket_id = 'books_pdf'
 );
 
 CREATE POLICY "Users can view their own files"
 ON storage.objects FOR SELECT TO authenticated USING (
-  bucket_id = 'files' AND OWNER = auth.uid()
+  bucket_id = 'books_pdf' AND OWNER = auth.uid()
 );
 
 CREATE POLICY "Users can update their own files"
 ON storage.objects FOR UPDATE TO authenticated WITH CHECK (
-  bucket_id = 'files' AND OWNER = auth.uid()
+  bucket_id = 'books_pdf' AND OWNER = auth.uid()
 );
 
 CREATE POLICY "Users can delete their own files"
 ON storage.objects FOR DELETE TO authenticated USING (
-  bucket_id = 'files' AND OWNER = auth.uid()
+  bucket_id = 'books_pdf' AND OWNER = auth.uid()
 );
 
 -- ======================================
